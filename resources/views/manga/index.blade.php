f9e3199aefea87c977d789d0050e3bfc91d01a6a
Kaishiyoku <dmp@nfze.de>
2018-04-08 13:36:36 +0200

    closed #28 enhanced manga list layout, especially on mobile devices

--- resources/views/manga/index.blade.php
+++ resources/views/manga/index.blade.php
@@ -15,68 +15,57 @@
         @endguest
     </p>
 
-    <div class="row">
-        <div class="col-lg-8 offset-lg-2">
-            @if ($mangas->count() == 0)
-                <p class="lead"><i>{{ __('manga.no_mangas_yet') }}</i></p>
-            @else
-                <table class="table table-bordered table-striped table-sm table-responsive-sm">
-                    <thead>
-                    <tr>
-                        <th>{{ __('validation.attributes.name') }}</th>
-                        <th>{{ __('manga.index.mal_score') }}</th>
-                        <th>{{ __('validation.attributes.is_completed') }}</th>
-                        <th>{{ __('validation.attributes.volume') }}</th>
-                    </tr>
-                    </thead>
+    @if ($mangas->count() == 0)
+        <p class="lead"><i>{{ __('manga.no_mangas_yet') }}</i></p>
+    @else
+        <div>
+            <div class="card-columns">
+                @foreach ($mangas as $manga)
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $manga->name }}
 
-                    <tbody>
-                    @foreach ($mangas as $manga)
-                        @foreach ($manga->volumes as $i => $volume)
-                            <tr class="{{ $i == $manga->volumes->count() - 1  ? 'bordered-bold' : ''}}">
-                                <td>
-                                    @if ($i == 0)
-                                        <strong>{{ $manga->name }}</strong>
+                                @if ($manga->malItem && $manga->malItem->link_canonical)
+                                    {!! Html::decode(Html::link($manga->malItem->link_canonical, '<i class="fas fa-external-link"></i>', ['title' => __('manga.index.manga_at_myanimelist', ['name' => $manga->name])])) !!}
+                                @endif
+                            </h5>
 
-                                        @if ($manga->malItem != null)
-                                            {!! Html::decode(Html::link($manga->malItem->link_canonical, '<i class="fas fa-external-link"></i>', ['title' => __('manga.index.manga_at_myanimelist', ['name' => $manga->name])])) !!}
-                                        @endif
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($i == 0 && $manga->malItem != null)
-                                        {{ formatNumber($manga->malItem->score, 2) }}
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($i == 0)
-                                        {{ formatBool($manga->is_completed) }}
-                                    @endif
-                                </td>
-                                <td>{{ $volume->no }}</td>
-                            </tr>
-                        @endforeach
+                            @if ($manga->malItem && $manga->malItem->link_canonical)
+                                <div>{{ __('manga.index.mal_score') }}: {{ formatNumber($manga->malItem->score, 2) }}</div>
+                            @endif
 
-                        @foreach ($manga->specials as $i => $special)
-                            <tr class="{{ $i == $manga->specials->count() - 1  ? 'bordered-bold' : ''}}">
-                                <td>
-                                    @if ($i == 0)
-                                        <strong>{{ $manga->name }}</strong>
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($i == 0 && $manga->malItem != null)
-                                        {{ formatNumber($manga->malItem->score, 2) }}
-                                    @endif
-                                </td>
-                                <td></td>
-                                <td>{{ $special->name }}</td>
-                            </tr>
-                        @endforeach
-                    @endforeach
-                    </tbody>
-                </table>
-            @endif
+                            <div class="pb-3">
+                                {{ __('validation.attributes.is_completed') }}: {{ formatBool($manga->is_completed) }}
+                            </div>
+
+                            @if ($manga->malItem && $manga->malItem->image_url)
+                                <div class="text-center">
+                                    {{ Html::image($manga->malItem->image_url, $manga->name, ['class' => 'img-fluid']) }}
+                                </div>
+                            @endif
+
+                            <div>
+                                <table class="table table-striped table-sm mt-4">
+                                    <tbody>
+                                    @foreach ($manga->volumes as $volume)
+                                        <tr>
+                                            <td>{{ $volume->no }}</td>
+                                        </tr>
+                                    @endforeach
+
+                                    @foreach ($manga->specials as $special)
+                                        <tr>
+                                            <td>{{ $special->name }}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
