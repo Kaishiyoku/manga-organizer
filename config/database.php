ca2c0f06362a008c11658f49171ba07ea2e20be9
Andreas Wiedel <dmp@nfze.de>
2018-07-04 12:01:38 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -53,6 +53,18 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+        
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
