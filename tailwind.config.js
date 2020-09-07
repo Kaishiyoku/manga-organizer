e4a72997c94c7280cf87319d57f72fdb36ecd4b0
Kaishiyoku <dmp@nfze.de>
2020-09-07 22:14:13 +0200

    adjusted button and input focus outlines

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  purge: [
-    './resources/views/**/*.blade.php',
-    './resources/css/**/*.css',
-  ],
-  theme: {
-    extend: {
-        fontFamily: {
-            sans: ['Nunito', 'sans-serif'],
+    purge: [
+        './resources/views/**/*.blade.php',
+        './resources/css/**/*.css',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', 'sans-serif'],
+            },
         },
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/ui'),
-  ],
+    variants: {},
+    plugins: [
+        require('@tailwindcss/ui'),
+        require('tailwindcss-shadow-outline-colors')(),
+    ],
 }
