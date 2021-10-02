aac3f8c8ac76107275e4e2905bdef999323ac4e7
Kaishiyoku <dmp@nfze.de>
2021-10-02 13:14:24 +0200

    added database migrations

---
+++ database/migrations/2018_08_16_204821_rename_link_canonical_to_url_at_mal_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameLinkCanonicalToUrlAtMalItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mal_items', function (Blueprint $table) {
+            $table->renameColumn('link_canonical', 'url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mal_items', function (Blueprint $table) {
+            $table->renameColumn('url', 'link_canonical');
+        });
+    }
+}
