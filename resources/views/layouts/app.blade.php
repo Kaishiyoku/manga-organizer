38d06ee2bc984c897fa2da15fefca5284521f81f
Kaishiyoku <dmp@nfze.de>
2021-10-02 18:40:56 +0200

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        @include('shared._favicon')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
