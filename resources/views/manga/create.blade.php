2baaf4af58d6e2da4d9fa9197784ca702e8de6fc
Kaishiyoku <dmp@nfze.de>
2021-10-06 22:21:12 +0200

    added the ability to add mangas

---
+++ resources/views/manga/create.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Add manga') }}
+        </h2>
+    </x-slot>
+
+    <x-card.card>
+        <x-card.body>
+            {{ html()->modelForm($manga, 'post', route('mangas.store'))->open() }}
+                @include('manga._form_fields')
+
+                <x-button>
+                    {{ __('Save') }}
+                </x-button>
+            {{ html()->closeModelForm() }}
+        </x-card.body>
+    </x-card.card>
+
+    <div class="mt-8">
+        <x-link :href="route('mangas.manage')">
+            {{ __('Back') }}
+        </x-link>
+    </div>
+</x-app-layout>
