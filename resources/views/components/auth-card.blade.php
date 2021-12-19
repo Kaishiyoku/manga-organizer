faaa4c036c787dc763709535cddfb0efe4f81b35
Kaishiyoku <dmp@nfze.de>
2021-12-19 23:59:11 +0100

    adjusted ring opacities

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -3,7 +3,7 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 dark:bg-opacity-50 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
