b8f11284173c39b57ef4793598a40200aa67dae1
Kaishiyoku <dmp@nfze.de>
2022-04-02 18:49:30 +0200

    adjusted button styles

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center space-x-2 px-4 py-2 bg-gray-800 dark:bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-gray-600 active:bg-gray-900 dark:active:bg-gray-500 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center space-x-2 px-4 py-2 bg-purple-500 dark:bg-purple-600 border border-transparent rounded-md font-semibold shadow text-xs text-white uppercase tracking-widest hover:bg-purple-700 dark:hover:bg-purple-500 active:bg-gray-900 dark:active:bg-purple-800 focus:outline-none focus:border-gray-900 focus:ring focus:ring-purple-300/75 dark:focus:ring-purple-400/75 disabled:opacity-25 transition']) }}>
     {{ $slot }}
 </button>
