2d555fbb282972c6c7a23316dc7dac08588a5bb0
Kaishiyoku <dmp@nfze.de>
2022-12-19 18:31:04 +0100

    adjusted link component

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,5 +1,8 @@
 @props(['href', 'active' => false])
 
-<a href="{{ $href }}" class="{{ classNames('text-purple-600 dark:text-purple-400 border-b-2 border-transparent hover:border-purple-200 dark:hover:border-purple-800 transition', ['border-purple-300 dark:border-purple-700' => $active]) }}">
+<a
+    href="{{ $href }}"
+    @class(['text-purple-600 dark:text-purple-400 underline transition decoration-transparent hover:decoration-purple-600 dark:hover:decoration-purple-400', '' => $active])
+>
     {{ $slot }}
 </a>
